68ffeecbd47b31787234759a3ca33b2d772dd9ec
chandima <chandimar.online@gmail.com>
2023-11-21 23:29:20 +0530

    feat: add create tour plan 🛩️

--- src/app/(app)/locations/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ViewLocation(){
-    return(
-        <div>
-            <h1>View Location</h1>
-        </div>
-    )
-}
--- src/components/locations/locationData.ts
+++ src/components/locations/locationData.ts
@@ -1,261 +1,293 @@
 export const locations = [
-    {
-      "id": 1,
-      "name": "New York City",
-      "description": "The city that never sleeps, known for Times Square and Central Park.",
-      "coordinates": {
-        "latitude": 40.7128,
-        "longitude": -74.0060
-      },
-      "image": "/images/locations/location1.jpg",
-      "rating": 4.8,
-      "attractions": [
-        "Times Square",
-        "Central Park",
-        "Empire State Building"
-      ],
-      "hotels": [
-        {
-          "name": "Luxury Manhattan Hotel",
-          "rating": 4.6,
-          "price_range": "$$$",
-          "image_url": "https://example.com/luxury_manhattan_hotel.jpg",
-          "facilities": ["Spa", "Fitness Center", "Fine Dining"]
-        },
-        {
-          "name": "Cozy Midtown Inn",
-          "rating": 3.9,
-          "price_range": "$$",
-          "image_url": "https://example.com/cozy_midtown_inn.jpg",
-          "facilities": ["Free Wi-Fi", "Breakfast Included"]
-        }
-      ]
+  {
+    "id": 1,
+    "name": "New York City",
+    "description": "The city that never sleeps, known for Times Square and Central Park.",
+    "coordinates": {
+      "latitude": 40.7128,
+      "longitude": -74.0060
     },
-    {
-      "id": 2,
-      "name": "Barcelona",
-      "description": "A city with unique architecture by Antoni Gaudí and beautiful beaches.",
-      "coordinates": {
-        "latitude": 41.3851,
-        "longitude": 2.1734
+    "image": "/images/locations/location1.jpg",
+    "rating": 4.8,
+    "attractions": [
+      "Times Square",
+      "Central Park",
+      "Empire State Building"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Luxury Manhattan Hotel",
+        "rating": 4.6,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel0.jpg",
+        "facilities": ["Spa", "Fitness Center", "Fine Dining"],
+        "description": "Luxurious hotel with state-of-the-art facilities and breathtaking views of the city."
       },
-      "image": "/images/locations/location2.jpg",
-      "rating": 4.5,
-      "attractions": [
-        "Sagrada Familia",
-        "Park Güell",
-        "Barri Gòtic"
-      ],
-      "hotels": [
-        {
-          "name": "Modernist Luxury Hotel",
-          "rating": 4.7,
-          "price_range": "$$$",
-          "image_url": "https://example.com/modernist_luxury_hotel.jpg",
-          "facilities": ["Rooftop Pool", "Spa", "Michelin Star Restaurant"]
-        },
-        {
-          "name": "Charming Gothic Quarter Inn",
-          "rating": 4.0,
-          "price_range": "$$",
-          "image_url": "https://example.com/charming_gothic_inn.jpg",
-          "facilities": ["Free Bicycle Rental", "Courtyard"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Cozy Midtown Inn",
+        "rating": 3.9,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel1.jpg",
+        "facilities": ["Free Wi-Fi", "Breakfast Included"],
+        "description": "A charming inn offering a comfortable stay in the heart of Midtown Manhattan."
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Barcelona",
+    "description": "A city with unique architecture by Antoni Gaudí and beautiful beaches.",
+    "coordinates": {
+      "latitude": 41.3851,
+      "longitude": 2.1734
     },
-    {
-      "id": 3,
-      "name": "Sydney",
-      "description": "A stunning harbor city with the iconic Sydney Opera House and Bondi Beach.",
-      "coordinates": {
-        "latitude": -33.8688,
-        "longitude": 151.2093
+    "image": "/images/locations/location2.jpg",
+    "rating": 4.5,
+    "attractions": [
+      "Sagrada Familia",
+      "Park Güell",
+      "Barri Gòtic"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Modernist Luxury Hotel",
+        "rating": 4.7,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel3.jpg",
+        "facilities": ["Rooftop Pool", "Spa", "Michelin Star Restaurant"],
+        "description": "Experience luxury in the heart of Barcelona with a rooftop pool and Michelin-starred dining."
       },
-      "image": "/images/locations/location3.jpg",
-      "rating": 4.7,
-      "attractions": [
-        "Sydney Opera House",
-        "Bondi Beach",
-        "Harbour Bridge"
-      ],
-      "hotels": [
-        {
-          "name": "Harborview Luxury Hotel",
-          "rating": 4.5,
-          "price_range": "$$$",
-          "image_url": "https://example.com/harborview_luxury_hotel.jpg",
-          "facilities": ["Infinity Pool", "Waterfront Dining", "Spa"]
-        },
-        {
-          "name": "Seaside Boutique Inn",
-          "rating": 4.2,
-          "price_range": "$$",
-          "image_url": "https://example.com/seaside_boutique_inn.jpg",
-          "facilities": ["Ocean View Rooms", "Cozy Lounge"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Charming Gothic Quarter Inn",
+        "rating": 4.0,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel4.jpg",
+        "facilities": ["Free Bicycle Rental", "Courtyard"],
+        "description": "A cozy inn located in the historic Gothic Quarter, offering a blend of charm and modern comfort."
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Sydney",
+    "description": "A stunning harbor city with the iconic Sydney Opera House and Bondi Beach.",
+    "coordinates": {
+      "latitude": -33.8688,
+      "longitude": 151.2093
     },
-    {
-      "id": 4,
-      "name": "Kyoto",
-      "description": "An ancient city with traditional temples, gardens, and the historic Gion district.",
-      "coordinates": {
-        "latitude": 35.0116,
-        "longitude": 135.7681
+    "image": "/images/locations/location3.jpg",
+    "rating": 4.7,
+    "attractions": [
+      "Sydney Opera House",
+      "Bondi Beach",
+      "Harbour Bridge"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Harborview Luxury Hotel",
+        "rating": 4.5,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel5.jpg",
+        "facilities": ["Infinity Pool", "Waterfront Dining", "Spa"],
+        "description": "Indulge in luxury with stunning views of the harbor, an infinity pool, and waterfront dining."
       },
-      "image": "/images/locations/location4.jpg",
-      "rating": 4.6,
-      "attractions": [
-        "Kinkaku-ji",
-        "Fushimi Inari Shrine",
-        "Gion District"
-      ],
-      "hotels": [
-        {
-          "name": "Ryokan in the Historic Gion",
-          "rating": 4.6,
-          "price_range": "$$$",
-          "image_url": "https://example.com/ryokan_gion.jpg",
-          "facilities": ["Traditional Tatami Rooms", "Tea Ceremony"]
-        },
-        {
-          "name": "Modern Kyoto Inn",
-          "rating": 4.0,
-          "price_range": "$$",
-          "image_url": "https://example.com/modern_kyoto_inn.jpg",
-          "facilities": ["Contemporary Design", "Garden View"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Seaside Boutique Inn",
+        "rating": 4.2,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel6.jpg",
+        "facilities": ["Ocean View Rooms", "Cozy Lounge"],
+        "description": "A charming inn by the seaside, offering ocean view rooms and a cozy lounge for relaxation."
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Kyoto",
+    "description": "An ancient city with traditional temples, gardens, and the historic Gion district.",
+    "coordinates": {
+      "latitude": 35.0116,
+      "longitude": 135.7681
     },
-    {
-      "id": 5,
-      "name": "Cape Town",
-      "description": "A coastal city with Table Mountain and vibrant waterfront.",
-      "coordinates": {
-        "latitude": -33.918861,
-        "longitude": 18.423300
+    "image": "/images/locations/location4.jpg",
+    "rating": 4.6,
+    "attractions": [
+      "Kinkaku-ji",
+      "Fushimi Inari Shrine",
+      "Gion District"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Ryokan in the Historic Gion",
+        "rating": 4.6,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel7.jpg",
+        "facilities": ["Traditional Tatami Rooms", "Tea Ceremony"],
+        "description": "Experience traditional Japanese hospitality in a ryokan with tatami rooms and tea ceremonies."
       },
-      "image": "/images/locations/location5.jpg",
-      "rating": 4.3,
-      "attractions": [
-        "Table Mountain",
-        "Victoria & Alfred Waterfront",
-        "Cape of Good Hope"
-      ],
-      "hotels": [
-        {
-          "name": "Mountain View Resort",
-          "rating": 4.4,
-          "price_range": "$$$",
-          "image_url": "https://example.com/mountain_view_resort.jpg",
-          "facilities": ["Panoramic Views", "Outdoor Pool", "Spa"]
-        },
-        {
-          "name": "Harborfront Hotel",
-          "rating": 3.8,
-          "price_range": "$$",
-          "image_url": "https://example.com/harborfront_hotel.jpg",
-          "facilities": ["Harbor Views", "On-site Dining"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Modern Kyoto Inn",
+        "rating": 4.0,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel8.jpg",
+        "facilities": ["Contemporary Design", "Garden View"],
+        "description": "A modern inn with contemporary design and garden views, offering a unique stay in Kyoto."
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Cape Town",
+    "description": "A coastal city with Table Mountain and vibrant waterfront.",
+    "coordinates": {
+      "latitude": -33.918861,
+      "longitude": 18.423300
     },
-    {
-      "id": 6,
-      "name": "Venice",
-      "description": "A unique city built on canals, famous for St. Mark's Basilica and gondola rides.",
-      "coordinates": {
-        "latitude": 45.4408,
-        "longitude": 12.3155
+    "image": "/images/locations/location5.jpg",
+    "rating": 4.3,
+    "attractions": [
+      "Table Mountain",
+      "Victoria & Alfred Waterfront",
+      "Cape of Good Hope"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Mountain View Resort",
+        "rating": 4.4,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel9.jpg",
+        "facilities": ["Panoramic Views", "Outdoor Pool", "Spa"],
+        "description": "Enjoy panoramic views at a mountain-view resort with an outdoor pool and spa facilities."
       },
-      "image": "/images/locations/location6.jpg",
-      "rating": 4.5,
-      "attractions": [
-        "St. Mark's Basilica",
-        "Rialto Bridge",
-        "Grand Canal"
-      ],
-      "hotels": [
-        {
-          "name": "Canal-side Luxury Hotel",
-          "rating": 4.5,
-          "price_range": "$$$",
-          "image_url": "https://example.com/canal_side_luxury_hotel.jpg",
-          "facilities": ["Canal Views", "Venetian Decor", "Fine Dining"]
-        },
-        {
-          "name": "Charming Boutique Inn",
-          "rating": 4.1,
-          "price_range": "$$",
-          "image_url": "https://example.com/charming_boutique_inn.jpg",
-          "facilities": ["Quaint Courtyard", "Venetian Charm"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Harborfront Hotel",
+        "rating": 3.8,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel10.jpg",
+        "facilities": ["Harbor Views", "On-site Dining"],
+        "description": "A hotel with harbor views and on-site dining, providing a comfortable and convenient stay in Cape Town."
+      }
+    ]
+  },
+  {
+    "id": 6,
+    "name": "Venice",
+    "description": "A unique city built on canals, famous for St. Mark's Basilica and gondola rides.",
+    "coordinates": {
+      "latitude": 45.4408,
+      "longitude": 12.3155
     },
-    {
-      "id": 7,
-      "name": "Marrakech",
-      "description": "A vibrant city with bustling markets, palaces, and the Jardin Majorelle.",
-      "coordinates": {
-        "latitude": 31.6295,
-        "longitude": -7.9811
+    "image": "/images/locations/location6.jpg",
+    "rating": 4.5,
+    "attractions": [
+      "St. Mark's Basilica",
+      "Rialto Bridge",
+      "Grand Canal"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Canal-side Luxury Hotel",
+        "rating": 4.5,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel0.jpg",
+        "facilities": ["Canal Views", "Venetian Decor", "Fine Dining"],
+        "description": "Experience the charm of Venice in a canal-side luxury hotel with Venetian decor and fine dining."
       },
-      "image": "/images/locations/location7.jpg",
-      "rating": 4.4,
-      "attractions": [
-        "Jardin Majorelle",
-        "Medina Souks",
-        "Koutoubia Mosque"
-      ],
-      "hotels": [
-        {
-          "name": "Palatial Riad Hotel",
-          "rating": 4.6,
-          "price_range": "$$$",
-          "image_url": "https://example.com/palatial_riad_hotel.jpg",
-          "facilities": ["Moroccan Decor", "Courtyard Pool", "Spa"]
-        },
-        {
-          "name": "Centrally Located Inn",
-          "rating": 3.9,
-          "price_range": "$$",
-          "image_url": "https://example.com/centrally_located_inn.jpg",
-          "facilities": ["Convenient Location", "Traditional Design"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Charming Boutique Inn",
+        "rating": 4.1,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel1.jpg",
+        "facilities": ["Quaint Courtyard", "Venetian Charm"],
+        "description": "A charming boutique inn with a quaint courtyard, offering an authentic Venetian experience."
+      }
+    ]
+  },
+  {
+    "id": 7,
+    "name": "Marrakech",
+    "description": "A vibrant city with bustling markets, palaces, and the Jardin Majorelle.",
+    "coordinates": {
+      "latitude": 31.6295,
+      "longitude": -7.9811
     },
-    {
-      "id": 8,
-      "name": "Bangkok",
-      "description": "A bustling city known for ornate shrines, vibrant street life, and cultural landmarks.",
-      "coordinates": {
-        "latitude": 13.7563,
-        "longitude": 100.5018
+    "image": "/images/locations/location7.jpg",
+    "rating": 4.4,
+    "attractions": [
+      "Jardin Majorelle",
+      "Medina Souks",
+      "Koutoubia Mosque"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Palatial Riad Hotel",
+        "rating": 4.6,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel2.jpg",
+        "facilities": ["Moroccan Decor", "Courtyard Pool", "Spa"],
+        "description": "Experience the luxury of a palatial riad hotel with Moroccan decor, a courtyard pool, and spa facilities."
       },
-      "image": "/images/locations/location8.jpg",
-      "rating": 4.3,
-      "attractions": [
-        "Wat Arun",
-        "Grand Palace",
-        "Chatuchak Market"
-      ],
-      "hotels": [
-        {
-          "name": "Luxury Riverside Hotel",
-          "rating": 4.5,
-          "price_range": "$$$",
-          "image_url": "https://example.com/luxury_riverside_hotel.jpg",
-          "facilities": ["River Views", "Infinity Pool", "Fine Dining"]
-        },
-        {
-          "name": "Downtown Boutique Inn",
-          "rating": 4.0,
-          "price_range": "$$",
-          "image_url": "https://example.com/downtown_boutique_inn.jpg",
-          "facilities": ["Central Location", "Modern Design"]
-        }
-      ]
+      {
+        "id": 2,
+        "name": "Centrally Located Inn",
+        "rating": 3.9,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel3.jpg",
+        "facilities": ["Convenient Location", "Traditional Design"],
+        "description": "A centrally located inn with a convenient location and traditional Moroccan design."
+      }
+    ]
+  },
+  {
+    "id": 8,
+    "name": "Bangkok",
+    "description": "A bustling city known for ornate shrines, vibrant street life, and cultural landmarks.",
+    "coordinates": {
+      "latitude": 13.7563,
+      "longitude": 100.5018
     },
-    {
+    "image": "/images/locations/location8.jpg",
+    "rating": 4.3,
+    "attractions": [
+      "Wat Arun",
+      "Grand Palace",
+      "Chatuchak Market"
+    ],
+    "hotels": [
+      {
+        "id": 1,
+        "name": "Luxury Riverside Hotel",
+        "rating": 4.5,
+        "price_range": "$$$",
+        "image_url": "/images/hotels/hotel5.jpg",
+        "facilities": ["River Views", "Infinity Pool", "Fine Dining"],
+        "description": "A luxury riverside hotel with river views, an infinity pool, and fine dining experiences."
+      },
+      {
+        "id": 2,
+        "name": "Downtown Boutique Inn",
+        "rating": 4.0,
+        "price_range": "$$",
+        "image_url": "/images/hotels/hotel6.jpg",
+        "facilities": ["Central Location", "Modern Design"],
+        "description": "A boutique inn in downtown Bangkok with a central location and modern design."
+      }
+    ]
+  },
+  {
       "id": 9,
       "name": "Prague",
       "description": "A city with a rich history, known for Prague Castle and Charles Bridge.",
@@ -272,18 +304,22 @@ export const locations = [
       ],
       "hotels": [
         {
+          "id": 1,
           "name": "Castle View Luxury Hotel",
           "rating": 4.6,
           "price_range": "$$$",
-          "image_url": "https://example.com/castle_view_luxury_hotel.jpg",
-          "facilities": ["Castle Views", "Historic Elegance", "Gourmet Dining"]
+          "image_url": "/images/hotels/hotel7.jpg",
+          "facilities": ["Castle Views", "Historic Elegance", "Gourmet Dining"],
+          "description": "Experience the luxury of a palatial riad hotel with Moroccan decor, a courtyard pool, and spa facilities."
         },
         {
+            "id": 2,
           "name": "Historic District Inn",
           "rating": 4.1,
           "price_range": "$$",
-          "image_url": "https://example.com/historic_district_inn.jpg",
-          "facilities": ["Central Location", "Charming Atmosphere"]
+          "image_url": "/images/hotels/hotel8.jpg",
+          "facilities": ["Central Location", "Charming Atmosphere"],
+          "description": "A charming boutique inn with a quaint courtyard, offering an authentic Venetian experience."
         }
       ]
     },
@@ -304,18 +340,22 @@ export const locations = [
       ],
       "hotels": [
         {
+          "id": 1,
           "name": "Harborview Luxury Hotel",
           "rating": 4.7,
           "price_range": "$$$",
-          "image_url": "https://example.com/harborview_luxury_hotel_auckland.jpg",
-          "facilities": ["Harbor Views", "Rooftop Bar", "Spa"]
+          "image_url": "/images/hotels/hotel9.jpg",
+          "facilities": ["Harbor Views", "Rooftop Bar", "Spa"],
+          "description": "A luxury riverside hotel with river views, an infinity pool, and fine dining experiences."
         },
         {
+          "id": 2,
           "name": "Modern Waterfront Inn",
           "rating": 4.2,
           "price_range": "$$",
-          "image_url": "https://example.com/modern_waterfront_inn_auckland.jpg",
-          "facilities": ["Waterfront Location", "Contemporary Design"]
+          "image_url": "/images/hotels/hotel10.jpg",
+          "facilities": ["Waterfront Location", "Contemporary Design"],
+          "description": "A boutique inn in downtown Bangkok with a central location and modern design."
         }
       ]
     }
