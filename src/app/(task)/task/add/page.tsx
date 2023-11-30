a796fea02306d5360c76ac17bfb106d4dbafcfe6
Chandima Rathnayake <chandima@leafycode.com>
2023-11-30 21:45:25 +0530

    feat: upate add task ⭐

--- src/app/(task)/task/add/page.tsx
+++ src/app/(task)/task/add/page.tsx
@@ -64,25 +64,9 @@ export default function AddTask(){
     }
 
     return(
-        <div className="relative h-screen w-full flex items-center justify-center p-4">
-            <div
-                className="absolute top-10 left-10 h-[450px] w-[450px] rounded-full bg-orange-200 z-0"
-            />
-
-             <div
-                className="absolute left-100 top-50 h-[450px] w-[450px] rounded-full bg-blue-200 z-0"
-            />
-
-             <div
-                className="absolute bottom-10 right-10 h-[450px] w-[450px] rounded-full bg-pink-200 z-0"
-            />
-
-            <div
-                className="absolute top-0 left-0 right-0 bottom-0 z-30 backdrop-blur-[200px]"
-            />
-
-            <div className="relative h-auto min-h-[650px] max-w-[1000px] w-full shadow z-30 bg-white">
-                <div className={'h-8'}>
+        <div className="w-full flex items-center justify-center py-4 px-4">
+            <div className="h-auto w-full max-w-[1000px]">
+                <div className={''}>
                     <WizardHeader
                         currentStep={currentStep}
                         steps={steps}
