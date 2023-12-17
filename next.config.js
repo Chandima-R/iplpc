1f6351def5fc1a00238a641c6aa27574268a0f1e
Chandima Rathnayake <chandima@leafycode.com>
2023-12-18 02:47:10 +0530

    feat: update app flow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-    images:{
-        domains: ['images.unsplash.com', "images.unsplash.com"]
-    }
-}
+    images: {
+        domains: ['images.unsplash.com', 'images.unsplash.com'],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/login',
+                permanent: true,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
